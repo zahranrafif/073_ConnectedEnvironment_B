e67de582bba93e20ceab079fd507bf996aad064b
Zahran Rafif <zahranlumia@gmail.com>
2022-06-09 22:09:39 +0700

    Add project files.

---
+++ Connected1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connected1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = GetConnectionString();
+            string query1 = "select * from Pembimbing_Akademik where NIK=333";
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd1 = new SqlCommand(query1, cn); cn.Open();
+                using (SqlDataReader dr1 = cmd1.ExecuteReader())
+                {
+                    while(dr1.Read())
+                    {
+                        string query2 = "UPDATE Pembimbing_Akademik SET Keahlian = 'Jaringan' WHERE NIK = 333";
+                        SqlCommand cmd2 = new SqlCommand(query2, cn);
+                        cmd2.ExecuteNonQuery();
+                        Console.WriteLine("Data has been updated");
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+        private static string GetConnectionString()
+        {
+            return "data source = DESKTOP-8VLH0CG;database=ProdiTI;MultipleActiveResultSets=True;User ID=sa;Password = 1234";
+        }
+
+    }
+}
